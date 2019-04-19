43207d5cd578166dc7c91509ca52c925443168f0
sunjingyun <120208073@qq.com>
2019-04-19 19:14:27 +0800

    upgrade the wtctl

--- test/mocha-case.js
+++ test/mocha-case.js
@@ -9,33 +9,33 @@ describe("Array", function() {
             assert.equal([1, 2, 3].indexOf(4), -1);
         });
 
-        it("should return -1 when the value is not present - 2", function() {
-            assert.equal([1, 2, 3].indexOf(4), -1);
-            assert.equal([1, 2, 3].indexOf(4), -1);
-            assert.equal([1, 2, 3].indexOf(4), 0);
-            assert.equal([1, 2, 3].indexOf(4), -1);
-        });
+        // it("should return -1 when the value is not present - 2", function() {
+        //     assert.equal([1, 2, 3].indexOf(4), -1);
+        //     assert.equal([1, 2, 3].indexOf(4), -1);
+        //     assert.equal([1, 2, 3].indexOf(4), 0);
+        //     assert.equal([1, 2, 3].indexOf(4), -1);
+        // });
 
-        function sleep(duration) {
-            return new Promise((resolve, reject) => {
-                setTimeout(() => {
-                    return resolve();
-                }, duration);
-            });
-        }
+        // function sleep(duration) {
+        //     return new Promise((resolve, reject) => {
+        //         setTimeout(() => {
+        //             return resolve();
+        //         }, duration);
+        //     });
+        // }
 
-        it("should return -1 when the value is not present - 3", async function() {
-            await sleep(100000000);
+        // it("should return -1 when the value is not present - 3", async function() {
+        //     await sleep(100000000);
 
-            assert.equal([1, 2, 3].indexOf(4), -1);
-            assert.equal([1, 2, 3].indexOf(4), -1);
-            assert.equal([1, 2, 3].indexOf(4), 0);
-            assert.equal([1, 2, 3].indexOf(4), -1);
-        });
+        //     assert.equal([1, 2, 3].indexOf(4), -1);
+        //     assert.equal([1, 2, 3].indexOf(4), -1);
+        //     assert.equal([1, 2, 3].indexOf(4), 0);
+        //     assert.equal([1, 2, 3].indexOf(4), -1);
+        // });
 
-        it("should return -1 when the value is not present - 4"),
-            function() {
-                assert.equal(-1, [1, 2, 3].indexOf(5));
-            };
+        // it("should return -1 when the value is not present - 4"),
+        //     function() {
+        //         assert.equal(-1, [1, 2, 3].indexOf(5));
+        // };
     });
 });
